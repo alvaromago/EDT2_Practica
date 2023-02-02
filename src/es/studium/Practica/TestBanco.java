39e767ac666dc408e7313aff7e1c60721ac38be4
alvaromago <alvaromago>
2023-02-02 10:19:29 +0100

    Segundo commit

--- src/es/studium/Practica/TestBanco.java
+++ src/es/studium/Practica/TestBanco.java
@@ -1,38 +1,60 @@
 package es.studium.Practica;
 
 public class TestBanco {
+	/* Constantes extraídas */
+	private static final String EUROS = "euros.";
+	private static final String TIENE = " tiene ";
+	private static final String CUENTA = "La cuenta de ";
+
 	public static void main(String[] args) {
 		/* Antonio y Beatriz se hacen clientes del banco */
-		Cliente antonio = new Cliente("123456789Z", "Antonio Alonso", "Av. Pueblo Saharaui, s/n");
-		Cliente beatriz = new Cliente("987654321A", "Beatriz Benítez", "Calle Sol, 4");
+		Cliente antonio = antonio();
+		Cliente beatriz = beatriz();
 		/* Por defecto, todas las cuentas nuevas tienen 100€ */
 		Cuenta cuentaAntonio = new Cuenta(48151, 100, antonio);
 		Cuenta cuentaBeatriz = new Cuenta(62342, 100, beatriz);
+		/* Variables locales extraídas */
+		String nombreAntonio = cuentaAntonio.getCliente().getNombre();
+		String nombreBeatriz = cuentaBeatriz.getCliente().getNombre();
+		int saldoBeatriz = cuentaBeatriz.getSaldo();
+		int saldoAntonio = cuentaAntonio.getSaldo();
 		/* Antonio y Beatriz consultan el saldo */
-		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "+ cuentaAntonio.getSaldo() + "euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "+ cuentaBeatriz.getSaldo() + "euros.");
+		System.out.println(CUENTA + nombreAntonio + TIENE + saldoAntonio + EUROS);
+		System.out.println(CUENTA + nombreBeatriz + TIENE + saldoBeatriz + EUROS);
 		/* Beatriz transfiere 50€ a Antonio */
-		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() - 50);
-		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() + 50);
+		cuentaBeatriz.setSaldo(saldoBeatriz - 50);
+		cuentaAntonio.setSaldo(saldoAntonio + 50);
 		/*
 		 * Antonio y Beatriz vuelven a consultar su saldo para comprobar que todo ha ido
 		 * bien
 		 */
-		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "+ cuentaAntonio.getSaldo() + "euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "+ cuentaBeatriz.getSaldo() + "euros.");
+		System.out.println(CUENTA + nombreAntonio + TIENE + saldoAntonio + EUROS);
+		System.out.println(CUENTA + nombreBeatriz + TIENE + saldoBeatriz + EUROS);
 		/* Antonio gana 100€ en una rifa y lo ingresa en su cuenta */
-		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() + 100);
+		cuentaAntonio.setSaldo(saldoAntonio + 100);
 		/* Beatriz tiene que pagar 30€ a hacienda y retira el dinero */
-		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() - 30);
+		cuentaBeatriz.setSaldo(saldoBeatriz - 30);
 		/* Antonio y Beatriz consultan de nuevo el saldo de su cuenta. */
-		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "+ cuentaAntonio.getSaldo() + "euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "+ cuentaBeatriz.getSaldo() + "euros.");
+		System.out.println(CUENTA + nombreAntonio + TIENE + saldoAntonio + EUROS);
+		System.out.println(CUENTA + nombreBeatriz + TIENE + saldoBeatriz + EUROS);
 		/*
 		 * Antonio transfiere 50€ a Beatriz y vuelven a consultar el saldo de su cuenta
 		 */
-		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() - 50);
-		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() + 50);
-		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "+ cuentaAntonio.getSaldo() + "euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "+ cuentaBeatriz.getSaldo() + "euros.");
+		cuentaAntonio.setSaldo(saldoAntonio - 50);
+		cuentaBeatriz.setSaldo(saldoBeatriz + 50);
+		System.out.println(CUENTA + nombreAntonio + TIENE+ saldoAntonio + EUROS);
+		System.out.println(CUENTA + nombreBeatriz + TIENE+ saldoBeatriz + EUROS);
+	}
+
+	private static Cliente beatriz()
+	{
+		Cliente beatriz = new Cliente("987654321A", "Beatriz Benítez", "Calle Sol, 4");
+		return beatriz;
+	}
+
+	private static Cliente antonio()
+	{
+		Cliente antonio = new Cliente("123456789Z", "Antonio Alonso", "Av. Pueblo Saharaui, s/n");
+		return antonio;
 	}
 }
